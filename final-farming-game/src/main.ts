d2cc671741143696b5ad421a8317d26dcf9d32bf
Jane Tran <ttran161@ucsc.edu>
2023-11-27 14:35:35 -0800

    character's movement

--- final-farming-game/src/main.ts
+++ final-farming-game/src/main.ts
@@ -10,5 +10,41 @@ document.querySelector<HTMLDivElement>(
   "#ui"
 )!.innerHTML = `<p> Initialize Game </p>`;
 
-ctx!.fillStyle = "green";
-ctx!.fillRect(gameHeight / 2 - 75, gameWidth / 2 - 50, 150, 100);
+class Character {
+  constructor(public x: number, public y: number, public width: number, public height: number, public color: string) {}
+
+  draw() {
+    ctx!.fillStyle = this.color;
+    ctx!.fillRect(this.x, this.y, this.width, this.height);
+  }
+}
+
+// Initial character's position
+const farmer = new Character(gameWidth / 2 - 35, gameHeight / 2 - 35, 70, 70, "yellow");
+
+// Draw character
+function drawCharacter() {
+  ctx!.clearRect(0, 0, gameWidth, gameHeight);
+  farmer.draw();
+}
+
+// Arrow key presses
+document.addEventListener("keydown", (event) => {
+  switch (event.key) {
+    case "ArrowLeft":
+      farmer.x -= 10;
+      break;
+    case "ArrowRight":
+      farmer.x += 10;
+      break;
+    case "ArrowUp":
+      farmer.y -= 10;
+      break;
+    case "ArrowDown":
+      farmer.y += 10;
+      break;
+  }
+  drawCharacter();
+});
+
+drawCharacter();
