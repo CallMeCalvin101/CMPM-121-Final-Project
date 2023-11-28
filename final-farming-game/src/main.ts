736f195e1ccdeb10b07f8198460290ba2dd48853
CallMeCalvin101 <calvinkurniadjaja@gmail.com>
2023-11-27 23:13:21 -0800

    Implemented Scenario Class (F0.g)

--- final-farming-game/src/main.ts
+++ final-farming-game/src/main.ts
@@ -1,12 +1,11 @@
 import "./style.css";
+import { Scenario } from "./scenario.ts";
 
 const canvas = document.getElementById("game");
 const gameHeight = (canvas! as HTMLCanvasElement).height;
 const gameWidth = (canvas! as HTMLCanvasElement).width;
 
 const ctx = (canvas! as HTMLCanvasElement).getContext("2d");
-
-
 
 class Cell {
   isMoist: boolean;
@@ -24,38 +23,46 @@ class Cell {
   }
 }
 
-
-
 class Game {
   grid: Cell[][];
   weather: string; // 'sunny' or 'rainy'
 
   constructor() {
-    this.grid = Array.from({ length: 4 }, () => Array.from({ length: 4 }, () => new Cell()));
-    this.weather = 'sunny'; // 'sunny' or 'rainy'
+    this.grid = Array.from({ length: 4 }, () =>
+      Array.from({ length: 4 }, () => new Cell())
+    );
+    this.weather = "sunny"; // 'sunny' or 'rainy'
     this.updateWeatherUI();
   }
-//randomly change the weather conditions
+  //randomly change the weather conditions
   updateWeather() {
-    this.weather = Math.random() < 0.7 ? 'sunny' : 'rainy'; // 70% chance of sun
+    this.weather = Math.random() < 0.7 ? "sunny" : "rainy"; // 70% chance of sun
   }
-//update the weather condition text on screen
+  //update the weather condition text on screen
   updateWeatherUI() {
     const weatherElement = document.getElementById("weather");
     if (weatherElement) {
-      weatherElement.textContent = `Current Weather: ${this.weather.charAt(0).toUpperCase() + this.weather.slice(1)}`;
+      weatherElement.textContent = `Current Weather: ${
+        this.weather.charAt(0).toUpperCase() + this.weather.slice(1)
+      }`;
     }
   }
-//update the moisture of all grids
+  //update the moisture of all grids
   updateMoisture() {
-    if (this.weather === 'rainy') {
-      this.grid.forEach(row => row.forEach(cell => cell.isMoist = true));
+    if (this.weather === "rainy") {
+      this.grid.forEach((row) => row.forEach((cell) => (cell.isMoist = true)));
     } else {
-      this.grid.forEach(row => row.forEach(cell => cell.isMoist = cell.isMoist && !cell.driedYesterday));
+      this.grid.forEach((row) =>
+        row.forEach(
+          (cell) => (cell.isMoist = cell.isMoist && !cell.driedYesterday)
+        )
+      );
     }
-    this.grid.forEach(row => row.forEach(cell => cell.driedYesterday = !cell.isMoist));
+    this.grid.forEach((row) =>
+      row.forEach((cell) => (cell.driedYesterday = !cell.isMoist))
+    );
   }
-//placing any update functions here
+  //placing any update functions here
   updateGame() {
     this.updateWeather();
     this.updateWeatherUI();
@@ -67,7 +74,9 @@ class Grid {
   public cells: Cell[][];
 
   constructor(public rows: number, public cols: number) {
-    this.cells = Array.from({ length: rows }, () => Array.from({ length: cols }, () => new Cell()));
+    this.cells = Array.from({ length: rows }, () =>
+      Array.from({ length: cols }, () => new Cell())
+    );
     this.generateRandomGrid();
   }
 
@@ -114,7 +123,13 @@ function getColor(type: string): string {
 }
 
 class Character {
-  constructor(public x: number, public y: number, public width: number, public height: number, public color: string) {}
+  constructor(
+    public x: number,
+    public y: number,
+    public width: number,
+    public height: number,
+    public color: string
+  ) {}
 
   draw() {
     ctx!.fillStyle = this.color;
@@ -122,7 +137,13 @@ class Character {
   }
 }
 
-const farmer = new Character(gameWidth / 2 - 35, gameHeight / 2 - 35, 40, 70, "black");
+const farmer = new Character(
+  gameWidth / 2 - 35,
+  gameHeight / 2 - 35,
+  40,
+  70,
+  "black"
+);
 const gameGrid = new Grid(7, 7);
 const game = new Game();
 setInterval(() => {
@@ -138,7 +159,9 @@ function drawGame() {
 
   const weatherElement = document.getElementById("weather");
   if (weatherElement) {
-    weatherElement.textContent = `Current Weather: ${game.weather.charAt(0).toUpperCase() + game.weather.slice(1)}`;
+    weatherElement.textContent = `Current Weather: ${
+      game.weather.charAt(0).toUpperCase() + game.weather.slice(1)
+    }`;
   }
 }
 
@@ -150,10 +173,15 @@ class Plant {
   name: string;
   game: Game;
   cell: Cell | null; // can be null because it hasn't been planted yet
-  //for constructing a plant, 
+  //for constructing a plant,
   sunRequisite: number;
   waterRequisite: number;
-  constructor(game: Game,name:string,sunRequisite: number, waterRequisite: number) {
+  constructor(
+    game: Game,
+    name: string,
+    sunRequisite: number,
+    waterRequisite: number
+  ) {
     this.sunLevel = 0;
     this.waterLevel = 0;
     this.growthLevel = 0;
@@ -168,32 +196,34 @@ class Plant {
     //put a bang here cause "cell might be null"
     if (this.cell!.isMoist && Math.random() < 0.7) {
       this.waterLevel += 1;
-      console.log('Plant watered! Water level:', this.waterLevel);
+      console.log("Plant watered! Water level:", this.waterLevel);
     } else {
-      console.log('Watering failed. Water level remains the same.');
+      console.log("Watering failed. Water level remains the same.");
     }
   }
 
-
   exposeToSun() {
     // Simulate the effect of exposure to sun based on the game's weather
-    const sunChance = this.game.weather === 'sunny' ? 0.8 : 0.2;
-    
+    const sunChance = this.game.weather === "sunny" ? 0.8 : 0.2;
+
     if (Math.random() < sunChance) {
       this.sunLevel += 1;
-      console.log('Plant exposed to sun! Sun level:', this.sunLevel);
+      console.log("Plant exposed to sun! Sun level:", this.sunLevel);
     } else {
-      console.log('Exposure to sun failed. Sun level remains the same.');
+      console.log("Exposure to sun failed. Sun level remains the same.");
     }
   }
 
   grow(): void {
     // Simulate general growth based on accumulated sun and water levels
-    if (this.sunLevel >= this.sunRequisite && this.waterLevel >= this.waterRequisite) {
+    if (
+      this.sunLevel >= this.sunRequisite &&
+      this.waterLevel >= this.waterRequisite
+    ) {
       this.growthLevel += 1;
-      console.log('Plant is growing! Growth level:', this.growthLevel);
+      console.log("Plant is growing! Growth level:", this.growthLevel);
     } else {
-      console.log('No significant growth due to insufficient sun or water.');
+      console.log("No significant growth due to insufficient sun or water.");
     }
   }
 
@@ -204,7 +234,6 @@ class Plant {
     this.grow();
   }
 }
-
 
 //for debugging purposes, logs the cell and its properties
 function logCurrentCell() {
@@ -248,5 +277,23 @@ document.addEventListener("keydown", (event) => {
   drawGame();
 });
 
-
 drawGame();
+
+/*
+function runScenarioTest() {
+  const testCondition = "thisistest";
+  const testLoops = 3;
+  const testScenario = new Scenario(testCondition, testLoops);
+  for (let i = 0; i < testLoops; i++) {
+    if (testScenario.checkCondition(testCondition)) {
+      testScenario.increaseVal(1);
+    }
+  }
+
+  if (testScenario.checkTargetMet()) {
+    console.log("Scenario Class Test Passed");
+  }
+}
+
+runScenarioTest();
+*/
