8eac7b3d4ea4fd5ed013e3a0ba65731cdbdd021a
Jane Tran <ttran161@ucsc.edu>
2023-11-27 16:07:50 -0800

    make cell grid type

--- final-farming-game/src/main.ts
+++ final-farming-game/src/main.ts
@@ -10,6 +10,62 @@ document.querySelector<HTMLDivElement>(
   "#ui"
 )!.innerHTML = `<p> Initialize Game </p>`;
 
+class Grid {
+  private cells: string[][];
+
+  constructor(public rows: number, public cols: number) {
+    this.cells = new Array(rows).fill(null).map(() => new Array(cols).fill("empty"));
+    this.generateRandomGrid();
+  }
+
+  generateRandomGrid() {
+    for (let i = 0; i < this.rows; i++) {
+      for (let j = 0; j < this.cols; j++) {
+        const randomValue = Math.random();
+        if (randomValue < 0.25) {
+          this.cells[i][j] = "plant";
+        } else if (randomValue < 0.5) {
+          this.cells[i][j] = "water";
+        } else if (randomValue < 0.75) {
+          this.cells[i][j] = "sun";
+        } else {
+          this.cells[i][j] = "empty";
+        }
+      }
+    }
+  }
+
+  draw() {
+    const cellSize = gameWidth / this.cols;
+
+    for (let i = 0; i < this.rows; i++) {
+      for (let j = 0; j < this.cols; j++) {
+        const x = j * cellSize;
+        const y = i * cellSize;
+        const cellType = this.cells[i][j];
+
+        ctx!.fillStyle = getColor(cellType);
+        ctx!.fillRect(x, y, cellSize, cellSize);
+      }
+    }
+  }
+}
+
+function getColor(type: string): string {
+  switch (type) {
+    case "plant":
+      return "green";
+    case "water":
+      return "blue";
+    case "sun":
+      return "yellow";
+    case "empty":
+      return "#563d2d";
+    default:
+      return "#563d2d";
+  }
+}
+
 class Character {
   constructor(public x: number, public y: number, public width: number, public height: number, public color: string) {}
 
@@ -19,16 +75,16 @@ class Character {
   }
 }
 
-// Initial character's position
-const farmer = new Character(gameWidth / 2 - 35, gameHeight / 2 - 35, 70, 70, "yellow");
+const farmer = new Character(gameWidth / 2 - 35, gameHeight / 2 - 35, 40, 70, "black");
+const gameGrid = new Grid(5, 5);
 
-// Draw character
-function drawCharacter() {
+// Draw game
+function drawGame() {
   ctx!.clearRect(0, 0, gameWidth, gameHeight);
+  gameGrid.draw();
   farmer.draw();
 }
 
-// Arrow key presses
 document.addEventListener("keydown", (event) => {
   switch (event.key) {
     case "ArrowLeft":
@@ -44,7 +100,7 @@ document.addEventListener("keydown", (event) => {
       farmer.y += 10;
       break;
   }
-  drawCharacter();
+  drawGame();
 });
 
-drawCharacter();
+drawGame();
